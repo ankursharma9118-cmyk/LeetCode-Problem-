4dc483111000a83d345287d0c457e5cc45dfbe84
Ankur sharma <ankursharma9118@gmail.com>
2026-09-22 10:46:45 +0530

    Time: 0 ms (100%), Space: 27.5 MB (5.28%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -2,12 +2,14 @@ class Solution {
 public:
     void reverseString(vector<char>& s) {
         int left = 0;
-        int right = s.size() - 1;
-
+      int right = s.size() - 1;
         while (left < right) {
+     
             std::swap(s[left], s[right]);
             left++;
+     
             right--;
+    
         }
     }
 };
